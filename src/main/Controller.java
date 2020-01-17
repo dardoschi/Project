9f86b1958491439011a273daaef5e5808b9259d1
dardoschi <davidespada420@gmail.com>
2020-01-17 13:47:25 +0100

    added external jar driver for postgres (jdbc)

--- src/main/Controller.java
+++ src/main/Controller.java
@@ -11,7 +11,7 @@ public class Controller {
 	
 	//Constructor
 	public Controller() {
-		conn = new ConnectionFactory(this);
 		IDao = new ItemDao(this);
+		conn = new ConnectionFactory(this);
 	}
 }
