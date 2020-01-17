e13348bb07357d3972173624a5d72916b47fc706
dardoschi <davidespada420@gmail.com>
2020-01-17 12:37:00 +0100

    Created Item Dao, removed useless things from Driver, linked ItemDao to other classes

--- src/main/Controller.java
+++ src/main/Controller.java
@@ -3,9 +3,10 @@ package main;
 public class Controller {
 	
 	private ConnectionFactory conn;
-	
+	private ItemDao IDao;
 	//Constructor
 	public Controller() {
 		conn = new ConnectionFactory(this);
+		IDao = new ItemDao(this);
 	}
 }
--- src/main/Driver.java
+++ src/main/Driver.java
@@ -5,10 +5,6 @@ public class Driver {
 	public static void main(String[] args) {
 		
 		Controller MainController = new Controller();
-		System.out.println("prova");
-		int a = 3;
-		System.out.println("prov3a");
-		System.out.println("prova2");
 	}
 	
 
